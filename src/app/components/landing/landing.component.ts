fcca9c243488bdea90b6b23a7ea3aca779ddba21
shalevdev <shalevVarietyDeveloper@gmail.com>
2024-08-05 11:23:34 +0300

    complete lading page

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SocialMediaComponent } from "./components/social-media/social-media.component";
+import { BubbleComponent } from "./components/bubble/bubble.component";
+import { LogoComponent } from "./components/logo/logo.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, SocialMediaComponent, BubbleComponent, LogoComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
