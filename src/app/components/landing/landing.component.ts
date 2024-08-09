c1711ce8cfae370373ff589bfc9094e104bef9a9
shalevdev <shalevVarietyDeveloper@gmail.com>
2024-08-09 15:32:51 +0300

    add background elements

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { SocialMediaComponent } from "./components/social-media/social-media.component";
 import { BubbleComponent } from "./components/bubble/bubble.component";
-import { LogoComponent } from "./components/logo/logo.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [NavbarComponent, SocialMediaComponent, BubbleComponent, LogoComponent],
+  imports: [NavbarComponent, SocialMediaComponent, BubbleComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
