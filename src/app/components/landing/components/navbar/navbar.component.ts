083c8592db8f27e8451370ad810878868af72c39
shalevdev <shalevVarietyDeveloper@gmail.com>
2024-08-14 08:18:44 +0300

    responsive navbar

--- src/app/components/landing/components/navbar/navbar.component.ts
+++ src/app/components/landing/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements AfterViewInit{
+  @ViewChild('wrapper') wrapper!: ElementRef<HTMLElement>;
+  readonly navItemClass = 'nav-item';
+  ngAfterViewInit(): void{
+    let wrapperChildren: Element[] = Array.from(this.wrapper.nativeElement.children);
+    let navItems = wrapperChildren.filter(wrapperChild => wrapperChild.classList.contains(this.navItemClass));
+    navItems.forEach(navItem => {
+      navItem.innerHTML = navItem.innerHTML
+      .split(' ')      
+      .join(' <br class="dynamic-new-line">');
+    });
+  }
+  
 }
