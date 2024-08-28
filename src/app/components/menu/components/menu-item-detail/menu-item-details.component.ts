bb65b651892573c3b7ef790f0b28cf6371e8f519
Shalev Tamir <shalevtamir1009@gmail.com>
2024-08-28 21:13:46 +0300

    create menu item details component

---
+++ src/app/components/menu/components/menu-item-detail/menu-item-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { localPhoneNumber } from '@root/app/models/contact-data';
+import { CardData } from '../../models/card-data';
+
+@Component({
+  selector: 'app-menu-item-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-item-details.component.html',
+  styleUrl: './menu-item-details.component.scss'
+})
+export class MenuItemDetailComponent {
+  @Input({ required: true }) cardData!: CardData
+  protected readonly localPhoneNumber: string = localPhoneNumber;
+}
