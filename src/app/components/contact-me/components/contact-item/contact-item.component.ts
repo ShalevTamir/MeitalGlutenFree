35e0b1747a6d55a2e45b67067284afe9c7d6e143
shalevdev <shalevVarietyDeveloper@gmail.com>
2024-10-01 19:24:59 +0300

    create contact item component and extract social links to separate file

--- src/app/components/contact-me/components/contact-item/contact-item.component.ts
+++ src/app/components/contact-me/components/contact-item/contact-item.component.ts
@@ -1,15 +1,14 @@
-import { Component} from '@angular/core';
-import { internationalPhoneNumber, localPhoneNumber } from '@root/common/models/contact-data';
+import { Component } from '@angular/core';
 import { callLink, facebookLink, instagramLink, whatsappLink } from '@root/common/models/socials-links';
 
 @Component({
-  selector: 'app-social-media',
+  selector: 'app-contact-item',
   standalone: true,
   imports: [],
-  templateUrl: './social-media.component.html',
-  styleUrl: './social-media.component.scss'
+  templateUrl: './contact-item.component.html',
+  styleUrl: './contact-item.component.scss'
 })
-export class SocialMediaComponent {
+export class ContactItemComponent {
   protected readonly whatsappLink = whatsappLink;
   protected readonly instagramLink = instagramLink;
   protected readonly facebookLink = facebookLink;
