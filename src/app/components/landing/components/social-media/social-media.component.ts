72c79ecdc4c1e1ac1454cd701271039979d65c80
shalevdev <shalevVarietyDeveloper@gmail.com>
2024-10-20 20:00:59 +0300

    add animation to socials bar disappear

--- src/app/components/landing/components/social-media/social-media.component.ts
+++ src/app/components/landing/components/social-media/social-media.component.ts
@@ -9,20 +9,38 @@ import { SocialData } from '@root/app/components/contact-me/models/social-data';
 import { NgFor } from '@angular/common';
 import { getParsedName, SocialType } from '@root/app/components/contact-me/models/enums/social-type.enum';
 import { BottomScrollDetector } from '@root/common/services/bottom-scroll-detector.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
+const animationDuration = '0.4s';
+const openStateStyle = style({
+  bottom: '0'
+});
+const closedStateStyle = style({
+  bottom: '-70px'
+});
+const animationConfig = animationDuration + ' ease-in-out';
 
 @Component({
   selector: 'app-social-media',
   standalone: true,
   imports: [NgFor],
   templateUrl: './social-media.component.html',
-  styleUrl: './social-media.component.scss'
+  styleUrl: './social-media.component.scss',
+  animations: [
+    trigger('openClose', [
+      state('open', openStateStyle),
+      state('closed', closedStateStyle),
+      transition('closed <=> open', [animate(animationConfig)])
+    ])
+  ]
 })
 export class SocialMediaComponent implements AfterViewInit {
   @ViewChild('wrapper') private _wrapper!: ElementRef<HTMLElement>;
 
   private _defaultDisplayValue: string = 'flex';
   protected socials: SocialData[];
-  protected getSocialTypeName: (socialType: SocialType) => string = getParsedName
+  protected getSocialTypeName: (socialType: SocialType) => string = getParsedName;
+  protected isOpen = true;
 
   constructor(socialDataManager: SocialDataManager, bottomScrollDetector: BottomScrollDetector){
     bottomScrollDetector.ReachedBottomEvent.subscribe(this.hideComponent.bind(this));
@@ -35,11 +53,15 @@ export class SocialMediaComponent implements AfterViewInit {
   }
 
   private hideComponent(){
-    this._wrapper.nativeElement.style.display = 'none';
+    // this._wrapper.nativeElement.style.display = 'none';
+    console.log('closing');
+    this.isOpen = false;
   }
 
   private showComponent(){
-    this._wrapper.nativeElement.style.display = this._defaultDisplayValue;
+    // this._wrapper.nativeElement.style.display = this._defaultDisplayValue;
+    console.log('opening');
+    this.isOpen = true;
   }
 
 }
