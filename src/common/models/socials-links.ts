5a23f8df633aba35014eccc246274cd0aff5ecec
Shalev Tamir <shalevtamir1009@gmail.com>
2024-10-22 12:10:45 +0300

    fix social links

--- src/common/models/socials-links.ts
+++ src/common/models/socials-links.ts
@@ -1,6 +1,6 @@
 import { internationalPhoneNumber, localPhoneNumber } from "./contact-data";
 
-export const whatsappLink = `https://api.whatsapp.com/send?phone=${{internationalPhoneNumber}}`;
+export const whatsappLink = `https://api.whatsapp.com/send?phone=${internationalPhoneNumber}`;
 export const instagramLink = `https://www.instagram.com/meital_gluten_free123/`;
 export const facebookLink = `https://www.facebook.com/profile.php?id=100005746929191`;
-export const callLink = `tel:${{localPhoneNumber}}`;
+export const callLink = `tel:${localPhoneNumber}`;
